d153bb529cbf49f275ed2296b8938d7bcf1d1462
Jaspaul Aggarwal <Jaspaul.Aggarwal@dixonscarphone.com>
2020-11-06 23:06:12 +0000

    Pusher

---
+++ src/config/prod.ts
@@ -0,0 +1,11 @@
+import { KeyProps } from "./keys";
+
+const keys: KeyProps = {
+    port: 80,
+    pusherAppId: "1103488",
+    pusherCluster: "eu",
+    pusherKey: "2b13af0c5367af0af7ac",
+    pusherSecret: "f9d32608af0d63e85acc"
+}
+
+module.exports = keys
